ce6b3a7ef5d7d097a0f0ad4a0f289b5944226aae
pawan pandey <pawan.pandey@fortunekit.com>
2018-04-22 16:58:32 +0530

    add mongoose support

--- src/index.js
+++ src/index.js
@@ -2,10 +2,12 @@ var config = require('./config');
 var find = require('./find');
 var findWithReq = require('./findWithReq');
 var search = require('./search');
+var mongoosePlugin = require('./mongoose.plugin');
 
 module.exports = {
   config,
   find,
   findWithReq,
-  search
+  search,
+  mongoosePlugin
 };
