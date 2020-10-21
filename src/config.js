a36f1f491117f567c11cc9c32e7aebb42edb0e36
Ryan Freebern <ryan@mixmax.com>
2020-10-21 13:22:55 -0400

    feat: increase MAX_LIMIT to allow larger page sizes

--- src/config.js
+++ src/config.js
@@ -2,7 +2,7 @@ module.exports = {
   /**
    * {Number} The maximum limit (page size).
    */
-  MAX_LIMIT: 300,
+  MAX_LIMIT: 5000,
 
   /**
    * {Number} The default limit (page size), if none is specified.
