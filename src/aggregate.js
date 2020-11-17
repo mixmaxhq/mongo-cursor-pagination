e5e832672f9ec0ba5e1bd393b11eef90ea10a0b8
Dom Scandinaro <scandinaro@gmail.com>
2020-11-16 18:30:31 -0600

    Update aggregate.js

--- src/aggregate.js
+++ src/aggregate.js
@@ -75,6 +75,8 @@ module.exports = async function aggregate(collection, params) {
   // https://www.npmjs.com/package/mongoist#cursor-operations
   const aggregateMethod = collection.aggregateAsCursor ? 'aggregateAsCursor' : 'aggregate';
   
+  console.log('collection[aggregateMethod](params.aggregation, { ...options, readPreference })', collection[aggregateMethod](params.aggregation, { ...options, readPreference }));
+  
   const results = await collection[aggregateMethod](params.aggregation, { ...options, readPreference }).toArray();
 
   return prepareResponse(results, params);
