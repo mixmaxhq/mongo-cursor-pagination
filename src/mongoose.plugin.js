0efd73c9a5e53887226a4a1d2b61605a0e168514
bergben <bergben@tutanota.com>
2021-02-25 12:25:31 +0100

    feat(mongoose-plugin): add search function

--- src/mongoose.plugin.js
+++ src/mongoose.plugin.js
@@ -1,4 +1,5 @@
 const find = require('./find');
+const search = require('./search');
 const _ = require('underscore');
 
 /**
@@ -6,27 +7,48 @@ const _ = require('underscore');
  * @param {Object} schema mongoose schema.
  * @param {Object} options
  * @param {string} options.name name of the function.
+ * @param {string} options.searchFnName name of the function.
  */
 
-module.exports = function(schema, options) {
+module.exports = function (schema, options) {
   /**
    * paginate function
-   * @param {Object} param required parameter
+   * @param {Object} params required parameter
    */
-
-  const fn = function(param) {
+  const findFn = function (params) {
     if (!this.collection) {
       throw new Error('collection property not found');
     }
 
-    param = _.extend({}, param);
+    params = _.extend({}, params);
 
-    return find(this.collection, param);
+    return find(this.collection, params);
+  };
+
+  /**
+   * search function
+   * @param {String} searchString String to search on. Required parameter
+   * @param {Object} params
+   */
+  const searchFn = function (searchString, params) {
+    if (!this.collection) {
+      throw new Error('collection property not found');
+    }
+
+    params = _.extend({}, params);
+
+    return search(this.collection, searchString, params);
   };
 
   if (options && options.name) {
-    schema.statics[options.name] = fn;
+    schema.statics[options.name] = findFn;
   } else {
-    schema.statics.paginate = fn;
+    schema.statics.paginate = findFn;
+  }
+
+  if (options && options.searchFnName) {
+    schema.statics[options.searchFnName] = searchFn;
+  } else {
+    schema.statics.search = searchFn;
   }
 };
