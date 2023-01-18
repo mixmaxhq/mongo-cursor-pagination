fbaa56498e4b2f73349729a0faebd5b020eb7774
Timothy Long <timothy@conpago.com.au>
2023-01-19 08:08:14 +1000

    Update search test setup and expectation format

--- test/search.test.ts
+++ test/search.test.ts
@@ -2,6 +2,7 @@ import { Db, ObjectId } from 'mongodb';
 import { MongoMemoryServer } from 'mongodb-memory-server';
 import { search } from '../src';
 import dbUtils from './support/db';
+import { decode } from '../src/utils/bsonUrlEncoding';
 
 const driver = process.env.DRIVER;
 
@@ -12,23 +13,12 @@ describe('search', () => {
     mongod = await dbUtils.start();
     db = await dbUtils.db(mongod, driver);
 
+    // create two collections with indexes for searching
     await Promise.all([
-      db.collection('test_paging_search').createIndex(
-        {
-          mytext: 'text',
-        },
-        {
-          name: 'test_index',
-        }
-      ),
-      db.collection('test_duplicate_search').createIndex(
-        {
-          mytext: 'text',
-        },
-        {
-          name: 'test_index',
-        }
-      ),
+      db.collection('test_paging_search').createIndex({ mytext: 'text' }, { name: 'test_index' }),
+      db
+        .collection('test_duplicate_search')
+        .createIndex({ mytext: 'text' }, { name: 'test_index' }),
     ]);
 
     await Promise.all([
@@ -62,51 +52,52 @@ describe('search', () => {
           group: 'one',
         },
       ]),
-      db.collection('test_duplicate_search').insertMany([
-        {
-          _id: new ObjectId(),
-          mytext: 'one',
-          counter: 1,
-        },
-        {
-          _id: new ObjectId(),
-          mytext: 'one',
-          counter: 2,
-        },
-        {
-          _id: new ObjectId(),
-          mytext: 'one',
-          counter: 3,
-        },
-        {
-          _id: new ObjectId(),
-          mytext: 'one two',
-          counter: 4,
-        },
-        {
-          _id: new ObjectId(),
-          mytext: 'one two',
-          counter: 5,
-        },
-        {
-          _id: new ObjectId(),
-          mytext: 'one two',
-          counter: 6,
-        },
-      ]),
+
+      db.collection('test_duplicate_search').insertMany(
+        [
+          {
+            mytext: 'one',
+            counter: 1,
+          },
+          {
+            mytext: 'one',
+            counter: 2,
+          },
+          {
+            mytext: 'one',
+            counter: 3,
+          },
+          {
+            mytext: 'one two',
+            counter: 4,
+          },
+          {
+            mytext: 'one two',
+            counter: 5,
+          },
+          {
+            mytext: 'one two',
+            counter: 6,
+          },
+        ]
+          // expected returned order is above, however when sorted by duplicate fields, the secondary field
+          // (in this case, the _id) is sorted from most the recent to oldest created. Thus, by reversing the
+          // array of documents and adding the _id, means that counter 1 document has the latest object id
+          .reverse()
+          .map((document) => ({ ...document, _id: new ObjectId() }))
+      ),
     ]);
   });
 
   afterAll(() => mongod.stop());
 
-  describe('basic usage', () => {
+  describe('query documents without duplicate values in the indexted field', () => {
     it('queries the first few pages', async () => {
       const collection = db.collection('test_paging_search');
+
       // First page of 2
       let res = await search(collection, 'one', {
-        fields: {
-          mytext: 1,
-        },
+        fields: { mytext: 1 },
         limit: 2,
       });
 
@@ -153,6 +144,138 @@ describe('search', () => {
       expect(res.results[2].mytext).toEqual('one two three four five six seven eight');
       expect(res.results[2].score).toEqual(0.5625);
       expect(res.next).toEqual(undefined);
+    });
+
+    it('paginates ONLY FORWARDS in ascending order on text search (need previous cursor value', async () => {
+      const collection = db.collection('test_paging_search');
+
+      ////////////////////// PAGE EXPECTATIONS ///////////////////////////////
+
+      const expectPageOne = (paginationReturn) => {
+        const {
+          results: documentResults,
+          next: nextCursor,
+          previous: previousCursor,
+        } = paginationReturn;
+
+        expect(typeof nextCursor).toEqual('string');
+        expect(previousCursor).toBeUndefined();
+
+        expect(documentResults).toHaveLength(3);
+        expect(documentResults[0]).toEqual(
+          expect.objectContaining({
+            mytext: 'one',
+            score: 1.1,
+          })
+        );
+        expect(documentResults[1]).toEqual(
+          expect.objectContaining({
+            mytext: 'one two',
+            score: 0.75,
+          })
+        );
+        expect(documentResults[2]).toEqual(
+          expect.objectContaining({
+            mytext: 'one two three',
+            score: 0.6666666666666666,
+          })
+        );
+
+        expect(decode(nextCursor)).toEqual([
+          0.6666666666666666,
+          new ObjectId(documentResults[2]._id),
+        ]);
+      };
+
+      const expectPageTwo = (paginationReturn) => {
+        const {
+          results: documentResults,
+          next: nextCursor,
+        } = paginationReturn;
+
+        expect(typeof nextCursor).toEqual('string');
+
+        expect(documentResults).toHaveLength(3);
+        expect(documentResults[0]).toEqual(
+          expect.objectContaining({
+            mytext: 'one two three four',
+            score: 0.625,
+          })
+        );
+        expect(documentResults[1]).toEqual(
+          expect.objectContaining({
+            mytext: 'one two three four five',
+            score: 0.6,
+          })
+        );
+        expect(documentResults[2]).toEqual(
+          expect.objectContaining({
+            mytext: 'one two three four five six',
+            score: 0.5833333333333334,
+          })
+        );
+
+        expect(decode(nextCursor)).toEqual([
+          0.5833333333333334,
+          new ObjectId(documentResults[2]._id),
+        ]);
+      };
+
+      const expectPageThree = (paginationReturn) => {
+        const {
+          results: documentResults,
+          next: nextCursor,
+          previous: previousCursor,
+        } = paginationReturn;
+
+        expect(nextCursor).toBeUndefined();
+
+        expect(documentResults).toHaveLength(2);
+        expect(documentResults[0]).toEqual(
+          expect.objectContaining({
+            mytext: 'one two three four five six seven',
+            score: 0.5714285714285714,
+          })
+        );
+        expect(documentResults[1]).toEqual(
+          expect.objectContaining({
+            mytext: 'one two three four five six seven eight',
+            score: 0.5625,
+          })
+        );
+
+      };
+
+      /////////////////// PAGE 1 ///////////////////////
+
+      let response = await search(collection, 'one', {
+        fields: { mytext: 1 },
+        limit: 3,
+      });
+
+      expectPageOne(response);
+      const page1NextCursor = response.next;
+
+      ///////////// PAGE 2 (paginated forwards) /////////
+
+      response = await search(collection, 'one', {
+        fields: { mytext: 1 },
+        limit: 3,
+        next: page1NextCursor,
+      });
+
+      expectPageTwo(response);
+      const page2NextCursor = response.next;
+
+      ///////////// PAGE 3 (paginated forwards) /////////
+
+      response = await search(collection, 'one', {
+        fields: { mytext: 1 },
+        limit: 3,
+        next: page2NextCursor,
+      });
+
+      expectPageThree(response);
     });
   });
 
