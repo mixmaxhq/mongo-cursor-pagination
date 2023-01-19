f78d958253bba16d84856d75159efcfec7aff2a5
Timothy Long <timothy@conpago.com.au>
2023-01-19 11:45:05 +1000

    Simplify functionality and separate concerns

--- src/utils/sanitizeParams.ts
+++ src/utils/sanitizeParams.ts
@@ -1,7 +1,5 @@
-import { Collection } from 'mongodb';
-import _ from 'underscore';
+import { Collection, ObjectId } from 'mongodb';
 import { AggregateInputParams, AggregateParams, QueryInputParams, QueryParams } from '../types';
-
 import config from '../config';
 import { decode } from './bsonUrlEncoding';
 import getPropertyViaDotNotation from './getPropertyViaDotNotation';
@@ -10,85 +8,88 @@ export default async (
   collection: Collection | any,
   params: QueryInputParams | AggregateInputParams
 ): Promise<QueryParams | AggregateParams> => {
-  if (params.previous) params.previous = decode(params.previous as string);
-  if (params.next) params.next = decode(params.next as string);
-
-  params = _.defaults(params, {
-    limit: config.DEFAULT_LIMIT,
-    paginatedField: '_id',
-  });
-
-  if (params.limit < 1) params.limit = 1;
-  if (params.limit > config.MAX_LIMIT) params.limit = config.MAX_LIMIT;
-
-  // If the paginated field is not _id, then it might have duplicate values in it. This is bad
-  // because then we can't exclusively use it for our range queries (that use $lt and $gt). So
-  // to fix this, we secondarily sort on _id, which is always unique.
-  const shouldSecondarySortOnId = params.paginatedField !== '_id';
-
   //
-  // params.after - overides params.next
-  //
-  // The 'after' param sets the start position for the next page. This is similar to the
-  // 'next' param, with the difference that 'after' takes a plain _id instead of an encoded
-  // string of both _id and paginatedField values.
-  if (params.after) {
-    if (shouldSecondarySortOnId) {
-      // Since the primary sort field is not provided by the 'after' pagination cursor we
-      // have to look it up when the paginated field is not _id.
-      const doc = await collection.findOne(
-        { _id: params.after },
-        { [params.paginatedField]: true, _id: false }
-      );
-      if (doc) {
-        // Handle usage of dot notation in paginatedField
-        let prop = getPropertyViaDotNotation(params.paginatedField, doc);
-        if (params.sortCaseInsensitive) prop = prop.toLowerCase();
-        params.next = [prop, params.after];
-      }
-    } else {
-      params.next = params.after;
-    }
-  }
+  // set the params.paginatedField
+  params.paginatedField ??= '_id';
 
-  //
-  // params.before - overides params.previous
-  //
-  // The 'before' param sets the start position for the previous page. This is similar to the
-  // 'previous' param, with the difference that 'before' takes a plain _id instead of an encoded
-  // string of both _id and paginatedField values.
-  if (params.before) {
-    if (shouldSecondarySortOnId) {
-      // Since the primary sort field is not provided by the 'before' pagination cursor we
-      // have to look it up when the paginated field is not _id.
-      const doc = await collection.findOne(
-        { _id: params.before },
-        { [params.paginatedField]: true, _id: false }
-      );
-      if (doc) {
-        // Handle usage of dot notation in paginatedField
-        let prop = getPropertyViaDotNotation(params.paginatedField, doc);
-        if (params.sortCaseInsensitive) prop = prop.toLowerCase();
-        params.previous = [prop, params.before];
-      }
-    } else {
-      params.previous = params.before;
-    }
-  }
+  // set the params.limit
+  params.limit = (() => {
+    const requestedLimit = params.limit;
+    if (requestedLimit < 1) return 1;
+    if (requestedLimit > config.MAX_LIMIT) return config.MAX_LIMIT;
+    return requestedLimit || config.DEFAULT_LIMIT;
+  })();
 
-  // The query must always include the paginatedField so we can construct the cursor.
+  // set params.previous || params.next
+  await (async () => {
+    if (params.previous) params.previous = decode(params.previous as string);
+    if (params.next) params.next = decode(params.next as string);
+    // if after || before in params, overwrite an existing previous || next value
+    if (params.after || params.before) await applyAfterOrBeforeToParams({ collection, params });
+  })();
+
+  // set the params.fields, which are the requested projected fields PLUS the paginated field
+  // (the latter required for sorting and constucting the cursor)
   if (params.fields) {
-    params.fields = _.extend(
-      {
-        _id: 0, // Mongo includes this field by default, so don't request it unless the user wants it.
-      },
-      params.fields
-    );
-
-    if (!params.fields[params.paginatedField]) {
-      params.fields[params.paginatedField] = 1;
-    }
+    const { fields: requestedFields, paginatedField } = params;
+    params.fields = {
+      _id: 0, // mongo projects _id by default, so ensure only projecting if user requests
+      ...requestedFields,
+      [paginatedField]: 1,
+    };
   }
 
   return params;
 };
+
+/**
+ * @description
+ * The 'after' param sets the start position for the next page. This is similar to the
+ * 'next' param, with the difference that 'after' takes a plain _id instead of an encoded
+ * string of both _id and paginatedField values.
+ * > a valid params.after will override params.next value
+ *
+ * The 'before' param sets the start position for the previous page. This is similar to the
+ * 'previous' param, with the difference that 'before' takes a plain _id instead of an encoded
+ * string of both _id and paginatedField values.
+ * > a valid params.before will override params.previous value
+ *
+ * @returns undefined (but may update the given params.next || params.previous with a decoded cursor)
+ */
+async function applyAfterOrBeforeToParams({
+  collection,
+  params,
+}: {
+  collection: any;
+  params: QueryInputParams;
+}) {
+  const { after, before, sortCaseInsensitive, paginatedField } = params;
+  if ((after && before) || (!after && !before)) return;
+
+  // if the primary sort field is the _id, then the results are assured to have a unique
+  // value, and after || before can immediately overwrite the next || previous param
+  if (paginatedField === '_id') {
+    after ? (params.next = after) : (params.previous = before);
+    return;
+  }
+
+  // otherwise an alternative primary field may have duplicates affecting $gt | $lt sorts, so
+  // will need to be secondarily sorted by _id. As 'after' && 'before' cursors ONLY hold an _id
+  // value, the primary sort value needs to be established to create a valid next | previous cursor
+  const document = await collection.findOne(
+    { _id: new ObjectId(after || before) },
+    { [paginatedField]: true, _id: false }
+  );
+  if (!document) return;
+
+  // retrieve paginated field value (field can be single or dot-notation; such as "user.first_name")
+  const paginatedFieldValue = getPropertyViaDotNotation({
+    propertyName: paginatedField,
+    object: document,
+    sortCaseInsensitive,
+  });
+
+  // decoded next | previous cursor expection is [ <value of paginated field in document >, < _id of document >]
+  if (after) params.next = [paginatedFieldValue, after];
+  if (before) params.previous = [paginatedFieldValue, before];
+}
