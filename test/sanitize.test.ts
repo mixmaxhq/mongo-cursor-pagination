4db5f8e6df4f2a0537afb84797b7a84c4ade1019
Ashley McVeigh <ashley@conpago.com.au>
2023-08-04 13:50:31 +1000

    remove test that is no longer true

--- test/sanitize.test.ts
+++ test/sanitize.test.ts
@@ -13,43 +13,37 @@ describe('sanitize', () => {
   let paramsMulti;
 
   beforeEach(() => {
-    baseParams = {
-      next: 'WyJfX21peG1heF9fdW5kZWZpbmVkX18iLHsiJG9pZCI6IjY0YzMzOTlkNmNmYzc1YzNlMmY5ZDEyZCJ9XQ',
-      previous:
-        'WyJfX21peG1heF9fdW5kZWZpbmVkX18iLHsiJG9pZCI6IjY0YzMzOTlkNmNmYzc1YzNlMmY5ZDEyNiJ9XQ',
-      limit: 2,
-    };
-
     params = {
       paginatedField: 'created_at',
-      ...baseParams,
+      // ["created_at",{"$oid":"64cb29a1b2a41fc8221041b2"}]
+      next: 'WyJjcmVhdGVkX2F0Iix7IiRvaWQiOiI2NGNiMjlhMWIyYTQxZmM4MjIxMDQxYjIifV0=',
+      limit: 2,
     };
 
     paramsMulti = {
       paginatedFields: [{ paginatedField: 'created_at' }],
-      ...baseParams,
+      // {"created_at":"10-06-1992","_id":{"$oid":"64cb29a1b2a41fc8221041b2"}}
+      next: 'eyJjcmVhdGVkX2F0IjoiMTAtMDYtMTk5MiIsIl9pZCI6eyIkb2lkIjoiNjRjYjI5YTFiMmE0MWZjODIyMTA0MWIyIn19',
+      limit: 2,
     };
   });
 
-  it('sanitizie and sanitizeMulti do the same thing given the same arguments', async () => {
+  it('sanitize and sanitizeMulti do the same thing given the same arguments', async () => {
     const s = await sanitizeParams({}, params);
     const sm = await sanitizeMultiParamsMutate({}, paramsMulti);
     // these produce slightly different shapes, but the same data
     expect(s.paginatedField).toEqual(sm.paginatedFields![0].paginatedField);
   });
 
-  it('generateCursorQuery and generateCursorQueryMulti do the same thing given the same arguments', () => {
-    const cq = generateCursorQuery(params);
-    const cqm = generateCursorQueryMulti(paramsMulti);
-    expect(cq).toEqual(cqm);
-  });
-
   it('generateSort and generateSorts do the same thing given the same arguments', async () => {
     const sanitizedParams = await sanitizeParams({}, params);
     const sanitizedParamsMulti = await sanitizeMultiParamsMutate({}, paramsMulti);
 
+    // params multi adds the _id to the sort explicitly instead of tacking it on like the old one did.
+
     const s = generateSort(sanitizedParams);
     const sm = generateSorts(sanitizedParamsMulti);
-    expect(s).toEqual(sm);
+
+    expect(s?.created_at).toEqual(sm.created_at);
   });
 });
